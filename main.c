1dfb51e413b934c09b1195e91730f4c23ba71ffc
ThanhBV-0099 <68915054+ThanhBV-0099@users.noreply.github.com>
2020-11-03 22:26:34 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,344 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "wifip.h"
+#include "protocol.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+unsigned char use_touch,use_sensor;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+IWDG_HandleTypeDef hiwdg;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_IWDG_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_NVIC_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+uint16_t count_reset = 0,test=0;
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_IWDG_Init();
+  MX_USART1_UART_Init();
+
+  /* Initialize interrupts */
+  MX_NVIC_Init();
+  /* USER CODE BEGIN 2 */
+	//IWDG->KR = 0xAAAA; // Writing 0xAAAA in the Key register prevents watchdog reset
+//	IWDG->KR = 0xCCCC; // Start the independent watchdog timer
+		wifi_init();
+		HAL_GPIO_WritePin(TOUCH_DK_GPIO_Port,TOUCH_DK_Pin,GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(SW2_BLUE_GPIO_Port,SW2_BLUE_Pin,GPIO_PIN_SET);
+		HAL_GPIO_WritePin(LED_SENSOR_GPIO_Port,LED_SENSOR_Pin,GPIO_PIN_SET);
+	//	HAL_GPIO_WritePin(LED_WIFI_GPIO_Port,LED_WIFI_Pin,GPIO_PIN_SET);
+		use_touch=1;
+		use_sensor=0;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+		HAL_Delay(10);
+		if(count_reset >100) //250*10ms -> 250*(10+50)  = 8s
+		{ 
+		//	IWDG->KR = 0xAAAA;
+			//HAL_IWDG_Refresh(&hiwdg);
+			MX_IWDG_Init();
+			count_reset = 0;
+		}
+		else
+		{
+			count_reset++;
+		}
+		wifiprocess();//mat 50ms
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief NVIC Configuration.
+  * @retval None
+  */
+static void MX_NVIC_Init(void)
+{
+  /* USART1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(USART1_IRQn);
+}
+
+/**
+  * @brief IWDG Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_IWDG_Init(void)
+{
+
+  /* USER CODE BEGIN IWDG_Init 0 */
+
+  /* USER CODE END IWDG_Init 0 */
+
+  /* USER CODE BEGIN IWDG_Init 1 */
+
+  /* USER CODE END IWDG_Init 1 */
+  hiwdg.Instance = IWDG;
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_128;
+  hiwdg.Init.Window = 4095;
+  hiwdg.Init.Reload = 4095;
+  if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN IWDG_Init 2 */
+
+  /* USER CODE END IWDG_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_WIFI_Pin|LED_SENSOR_Pin|SW1_RED_Pin|SW1_BLUE_Pin 
+                          |SW2_RED_Pin|SW2_BLUE_Pin|ESP_RESET_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, TOUCH_DK_Pin|REALY2_Pin|RELAY1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_WIFI_Pin LED_SENSOR_Pin SW1_RED_Pin SW1_BLUE_Pin 
+                           SW2_RED_Pin SW2_BLUE_Pin ESP_RESET_Pin */
+  GPIO_InitStruct.Pin = LED_WIFI_Pin|LED_SENSOR_Pin|SW1_RED_Pin|SW1_BLUE_Pin 
+                          |SW2_RED_Pin|SW2_BLUE_Pin|ESP_RESET_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : TP2_Pin */
+  GPIO_InitStruct.Pin = TP2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(TP2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : TOUCH_DK_Pin REALY2_Pin RELAY1_Pin */
+  GPIO_InitStruct.Pin = TOUCH_DK_Pin|REALY2_Pin|RELAY1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : TP1_Pin */
+  GPIO_InitStruct.Pin = TP1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(TP1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SENSOR_IN_Pin */
+  GPIO_InitStruct.Pin = SENSOR_IN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(SENSOR_IN_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(char *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ wifip.c
@@ -0,0 +1,500 @@
+#include "wifip.h" 
+#include "wifi.h"
+#include "mcu_api.h" 
+//#include "protocol.h"
+
+#define TP1 							HAL_GPIO_ReadPin(TP1_GPIO_Port,TP1_Pin)
+#define TP2 							HAL_GPIO_ReadPin(TP2_GPIO_Port,TP2_Pin)
+#define SENSOR_IN 				HAL_GPIO_ReadPin(SENSOR_IN_GPIO_Port,SENSOR_IN_Pin)
+#define LED_SENSOR				LED_WIFI_GPIO_Port, LED_WIFI_Pin
+#define LED_WIFI					LED_SENSOR_GPIO_Port,LED_SENSOR_Pin
+#define SW1_BLUE					SW1_RED_GPIO_Port,SW1_RED_Pin
+#define SW1_RED						SW1_BLUE_GPIO_Port,SW1_BLUE_Pin
+#define SW2_BLUE					SW2_RED_GPIO_Port,SW2_RED_Pin
+#define SW2_RED						SW2_BLUE_GPIO_Port,SW2_BLUE_Pin
+#define TOUCH_DK 					TOUCH_DK_GPIO_Port,TOUCH_DK_Pin
+#define TB1								RELAY1_GPIO_Port,RELAY1_Pin
+#define TB2								RELAY2_GPIO_Port,RELAY2_Pin
+
+
+////////////////struct rf
+typedef struct
+{
+	uint8_t SOF;
+	uint8_t STATE_SW1;
+	uint8_t STATE_SW2;
+	uint8_t STATE_SW3;
+	uint8_t STATE_SW4;
+	uint64_t MAC;
+	uint8_t EOF1;
+	uint8_t EOF2;
+}FRAME_RECEIVE_RF;
+volatile FRAME_RECEIVE_RF Frame_Receive_Rf;
+extern UART_HandleTypeDef huart1;
+volatile uint8_t Nhanbuff=0,state_receive = 0;
+volatile uint8_t Nhanbuff_rf[200],Nhan_rf,state_receive_rf = 0,count_rf = 0;
+volatile unsigned char m;
+extern unsigned long countdown_1;
+extern unsigned char out_dl;
+void Uart_PutChar(unsigned char value)
+{
+	HAL_UART_AbortReceive_IT(&huart1);
+	HAL_UART_Transmit(&huart1,&value,1,100);
+	HAL_UART_Receive_IT(&huart1,&Nhanbuff,1);
+}
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if(huart->Instance==huart1.Instance)//nhan du lieu uart1
+	{
+		uart_receive_input(Nhanbuff);
+		HAL_UART_Receive_IT(&huart1,&Nhanbuff,1);
+	}
+}
+
+unsigned char wifi_state;
+
+volatile unsigned char State_switch_1;
+volatile unsigned char State_switch_2;
+volatile unsigned char State_switch_3;
+volatile unsigned char State_switch_4;
+volatile unsigned char State_sensor,run_countdown1,run_countdown2;
+volatile unsigned char time_sensor,light_led_ss=0,use_bt,dk_app;
+extern unsigned char use_touch,use_sensor,count_reset;
+uint16_t count_1s = 0;
+unsigned long State_countdown_1;
+unsigned long State_countdown_2;
+unsigned long State_countdown_3;
+unsigned long State_countdown_4;
+unsigned long State_thong_so1; // thong so add vao cong suat toi thieu 10 wh
+unsigned long State_thong_so2; // thong so dong dien don vi mA
+unsigned long State_thong_so3; // Thong so cong suat don vi chia 10 ra W
+unsigned long State_thong_so4; // thong so dien ap chia 10 ra vol
+unsigned long State_thong_so1_count; // thong so add vao cong suat toi thieu 10 wh
+unsigned long State_thong_so2_count; // thong so dong dien don vi mA
+unsigned long State_thong_so3_count; // Thong so cong suat don vi chia 10 ra W
+unsigned long State_thong_so4_count; // thong so dien ap chia 10 ra vol
+float diennang = 0;
+unsigned char dodienap = 0,nead_update_dienanng = 0;
+uint16_t count_nhay = 0,count_update = 0,count_setup = 0,time_count_setup = 0,count_reset_heart = 0;
+volatile uint16_t count_wifi_status = 0,count_blink_1s = 0,modeconfig = 0,timeout_config = 0,count_wifi_status_blink = 0,
+									old_pad1 = 0,old_pad2 = 0,old_pad3 = 0,old_pad4 = 0,count_config_wifi1 = 0,count_config_wifi2 = 0,state_config = 0,old_state1 = 0,
+									old_state2 = 0,old_state3 = 0,old_state4 = 0,timeout_update_rf = 0,count_reset_touch = 0,time_count_reset_touch = 0,flag_reset_touch = 0,
+									cycle_count_reset_touch = 0,time_led_wifi=0,time_led_sensor=0,state_led_mode_ss=0,time_update_all=0,check_sensor=0;
+	static uint8_t has_change_touchpad = 0,old_button = 0;
+	static uint8_t buff_send_rf[6]={'A','X','X','X','X','@'};
+
+void coundown_process(void)
+{
+	if(run_countdown2==1 )
+	{
+		if(use_bt==1)
+		{
+	if(count_1s >= 1000)
+	{
+		count_1s =0;
+		if(State_countdown_1 > 0 )
+		{	
+			if(State_countdown_1 >1)
+			{
+				State_countdown_1 --;
+			}
+			else //neu dung bang 1 thi togle thiet bi
+			{
+				State_switch_1 = 0;
+				mcu_dp_bool_update(DPID_SWITCH_1,State_switch_1);
+				State_countdown_1 = countdown_1;
+				State_sensor=0;
+				run_countdown1=0;
+				run_countdown2=0;
+				use_bt=0;	
+			}		
+	}
+}
+	else
+	{
+		count_1s ++;
+	}
+}
+		}
+	}
+
+
+void wifiprocess(void)
+{
+		wifi_uart_service();
+		wifi_state = mcu_get_wifi_work_state();
+		/////count cho update data;
+	if(time_update_all>1000)
+	{
+		all_data_update();
+		time_update_all=0;
+	}
+	else
+	{
+	time_update_all++;
+	}
+		HAL_UART_Receive_IT(&huart1,&Nhanbuff,1);
+//		//het timeout cua count setup
+//		
+		if(time_count_setup> 100)	//xoa gia tri sau 100 lan dem
+		{
+			count_setup = 0;
+			time_count_setup = 0;
+		}
+		else
+		{
+			time_count_setup++;
+		}
+		
+		//count cho blink cac che do
+		if(count_blink_1s >= 40) // nhan giu du 200 nhung doi blink lon hon "count_blink_1s" moi nhay led
+			// thoi gian dao trang thai led
+		{
+				count_blink_1s = 0;
+			if(modeconfig == 1)  // che do cho led nhay config 
+			{		
+				HAL_GPIO_WritePin(SW1_RED,GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(SW2_RED,GPIO_PIN_RESET);
+				//neu o cho do config thi nhay cac led len
+				if(timeout_config > 20)  // nhay trong 30 lan dem se thoat nhay led
+				{
+					modeconfig = 0;
+					timeout_config = 0;
+					HAL_GPIO_WritePin(SW2_RED,GPIO_PIN_SET);
+				}
+				else
+				{
+					timeout_config++;
+				}
+				HAL_GPIO_TogglePin(SW1_BLUE);
+				HAL_GPIO_TogglePin(SW2_BLUE);
+			}
+			else if(modeconfig==2)  // che do cho led nhay mode
+			{
+				HAL_GPIO_WritePin(LED_SENSOR,GPIO_PIN_RESET);
+				//neu o cho do config thi nhay cac led len
+				if(timeout_config > 10)  // nhay trong 30 lan dem se thoat nhay led
+				{
+					modeconfig = 0;
+					timeout_config = 0;
+					HAL_GPIO_WritePin(LED_WIFI,GPIO_PIN_RESET);
+				}
+				else
+				{
+					timeout_config++;
+				}
+				HAL_GPIO_TogglePin(LED_WIFI);	
+			}
+			else if(modeconfig==3)  // che do cho led nhay mode
+			{
+				HAL_GPIO_WritePin(LED_WIFI,GPIO_PIN_RESET);
+				//neu o cho do config thi nhay cac led len
+				if(timeout_config > 10)  // nhay trong 30 lan dem se thoat nhay led
+				{
+					modeconfig = 0;
+					timeout_config = 0;
+					use_sensor=1;
+					HAL_GPIO_WritePin(LED_SENSOR,GPIO_PIN_RESET);
+				}
+				else
+				{
+					timeout_config++;
+				}
+				HAL_GPIO_TogglePin(LED_SENSOR);
+			}
+			else
+			{
+				modeconfig = 0;
+				timeout_config = 0;
+			}
+		
+		}
+		else
+		{
+			count_blink_1s ++;
+		}
+		
+
+		
+//		//dinh ky reset touch 1 lan
+		if(cycle_count_reset_touch >= PERIOD_TO_RESET_TOUCH) //reset touch auto
+		{
+			cycle_count_reset_touch = 0;
+			flag_reset_touch = 1;
+		}
+		else
+		{
+			cycle_count_reset_touch ++;
+		}
+		
+		//cho can de reset touch
+		if(flag_reset_touch == 1)
+		{
+			//Cho nay can reset touch			
+			HAL_GPIO_WritePin(TOUCH_DK,GPIO_PIN_SET); // TAT touc
+			if(time_count_reset_touch >= 50)
+			{
+				time_count_reset_touch = 0;
+				flag_reset_touch = 2;
+				//cho nay can bat touch
+				HAL_GPIO_WritePin(TOUCH_DK,GPIO_PIN_RESET); // BAT touc
+			}
+			else
+			{
+				time_count_reset_touch++;
+			}
+		}
+		//cho can de reset touch
+		else if(flag_reset_touch == 2)
+		{
+			HAL_GPIO_WritePin(TOUCH_DK,GPIO_PIN_RESET); // BAT touch
+			if(time_count_reset_touch >= 50)
+			{
+				time_count_reset_touch = 0;
+				flag_reset_touch = 0;
+				//cho nay can bat touch
+			}
+			else
+			{
+				time_count_reset_touch++;
+			}
+		}
+		else
+		{
+			time_count_reset_touch = 0;
+		}		
+		//kiem tra nut 1
+		if(TP1 == GPIO_PIN_SET && time_count_reset_touch == 0 )
+		{				
+			//mcu_dp_bool_update(DPID_SWITCH_1,State_switch_1);// update trang thai nut len app
+			time_count_setup = 0;  //xoa count_reset sau 100 lan
+			cycle_count_reset_touch = 0;  // reset touch
+		//	count_reset=0; // reset MCU
+			count_reset_touch=0; //reset touch auto
+			if(count_setup == 1)//config mode
+			{
+				if(count_config_wifi1 > 200)
+				{
+				count_config_wifi1  = 200 ;
+				use_touch=1;
+				use_sensor=0;
+				modeconfig = 2; // che do cho nhay led luan phien
+				count_setup = 0;
+				}
+				else
+				{
+				count_config_wifi1 ++;
+				}
+			}	
+			
+			if(count_setup == 6)//config mode
+			{
+				if(count_config_wifi1 >200)
+				{
+				count_config_wifi1  = 200 ;
+				mcu_set_wifi_mode(0);
+				modeconfig = 1; // che do cho nhay led luan phien		
+				}
+				else
+				{
+				count_config_wifi1 ++;
+				}
+			}	
+			if(count_reset_touch >= 500 && flag_reset_touch == 0 )//TIME_NEED_TO_RESET_TOUCH && flag_reset_touch == 0 )//neu an giu lau qua mot khoan thoi gian co nghia la touch bi loi, can reset touch
+			{
+				count_reset_touch = TIME_NEED_TO_RESET_TOUCH;
+				flag_reset_touch = 1; // bat len de reset touch
+			}
+			else
+			{
+				count_reset_touch++;
+			}
+			if(old_pad1 == 0)
+			{
+				//reset count dem cho nhay
+				count_nhay = 0;
+				old_pad1 = 1;	
+				State_sensor=0;
+				run_countdown1=0;
+				if(State_switch_1 == 0)  
+				{
+					State_switch_1 = 1;
+				}
+				else
+				{
+					State_switch_1 = 0;
+				}
+				mcu_dp_bool_update(DPID_SWITCH_1,State_switch_1); // update trang thai len phan mem		
+				count_setup ++;
+			}
+		}
+	else
+		{
+			old_pad1 = 0;
+			count_config_wifi1  = 0;
+			count_reset_touch = 0;
+		}
+		//--------------------
+		
+		if(TP2 == GPIO_PIN_SET && time_count_reset_touch == 0 )
+		{				
+			//mcu_dp_bool_update(DPID_SWITCH_1,State_switch_1);// update trang thai nut len app
+			time_count_setup = 0;  //xoa count_reset sau 100 lan
+			cycle_count_reset_touch = 0;  // reset touch
+		//	count_reset=0; // reset MCU
+			count_reset_touch=0; //reset touch auto
+			if(count_setup == 1)//config mode
+			{
+				if(count_config_wifi2>200)
+				{
+				count_config_wifi2 =200 ;
+				use_touch=0;
+				modeconfig = 3; // che do cho nhay led luan phien
+				count_setup = 0;
+				}
+				else
+				{
+				count_config_wifi2++;
+				}
+			}	
+			
+				else if(count_setup == 6)//config mode
+			{
+				if(count_config_wifi2>200)
+				{
+				count_config_wifi2 =200 ;
+				mcu_set_wifi_mode(0);
+				modeconfig = 1; // che do cho nhay led luan phien
+				}
+				else
+				{
+				count_config_wifi2++;
+				}
+			}	
+			if(count_reset_touch >= 500 && flag_reset_touch == 0 )//TIME_NEED_TO_RESET_TOUCH && flag_reset_touch == 0 )//neu an giu lau qua mot khoan thoi gian co nghia la touch bi loi, can reset touch
+			{
+				count_reset_touch = TIME_NEED_TO_RESET_TOUCH;
+				flag_reset_touch = 1; // bat len de reset touch
+				count_reset=1;
+			}
+			else
+			{
+				count_reset_touch++;
+				count_reset=0;
+			}
+			if(old_pad2 == 0)
+			{
+				//reset count dem cho nhay
+				count_nhay = 0;
+				old_pad2 = 1;	
+				State_sensor=0;
+				run_countdown1=0;
+				if(state_led_mode_ss==0)
+				{
+					state_led_mode_ss=1;
+				}
+				else
+				{
+					state_led_mode_ss=0;
+				}
+				mcu_dp_bool_update(DPID_SWITCH_1,State_switch_1); // update trang thai len phan mem		
+				count_setup ++;
+			}
+		}
+		else
+		{
+			old_pad2 = 0;
+			count_config_wifi2 = 0;
+			count_reset_touch = 0;
+		}
+		
+		
+		//------------------------
+
+		if(use_sensor==1)
+		{
+			
+		if(state_led_mode_ss==0)
+		{
+			if(modeconfig == 0)
+			{
+				HAL_GPIO_WritePin(SW2_RED,GPIO_PIN_SET);
+				HAL_GPIO_WritePin(SW2_BLUE,GPIO_PIN_RESET);
+			}
+		}
+		else
+		{
+			if(modeconfig == 0)
+			{
+				HAL_GPIO_WritePin(SW2_RED,GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(SW2_BLUE,GPIO_PIN_SET);
+			}			
+		}
+			//---
+		if(SENSOR_IN == GPIO_PIN_SET)
+		{	
+				State_switch_1 = 1;
+				mcu_dp_bool_update(DPID_SWITCH_1,State_switch_1); // update trang thai len phan mem
+				use_bt=1;
+				check_sensor=1;
+				if(countdown_1==0 )
+			{
+				State_countdown_1=60;
+			}
+			else
+			{
+				State_countdown_1=countdown_1;
+			}	
+				if(time_sensor>=15)
+				{
+					HAL_GPIO_TogglePin(LED_SENSOR);
+						
+				//	HAL_GPIO_TogglePin(SW2_BLUE);
+				time_sensor=0;
+				}
+				else
+				{
+				time_sensor++;	
+				}
+			}
+		else
+		{	
+				HAL_GPIO_WritePin(LED_SENSOR,GPIO_PIN_SET);	
+				run_countdown2=1;	
+				check_sensor=0;
+		}
+	}
+		if(use_touch==1 || dk_app==1)
+		{
+		if( State_switch_1 == 1)   //relay
+		{
+			HAL_GPIO_WritePin(TB1,GPIO_PIN_SET);
+			if(modeconfig == 0)
+			{
+				dk_app=0;
+				HAL_GPIO_WritePin(SW1_RED,GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(SW1_BLUE,GPIO_PIN_SET);
+			}
+		}
+		else
+		{
+			HAL_GPIO_WritePin(TB1,GPIO_PIN_RESET);
+			if(modeconfig == 0)
+			{
+				dk_app=0;
+				HAL_GPIO_WritePin(SW1_RED,GPIO_PIN_SET);
+				HAL_GPIO_WritePin(SW1_BLUE,GPIO_PIN_RESET);
+			}
+		}
+	}
+}
+		
+void wifi_init(void)
+{
+	HAL_GPIO_WritePin(ESP_RESET_GPIO_Port,ESP_RESET_Pin,GPIO_PIN_RESET);
+	HAL_Delay(1);
+	HAL_GPIO_WritePin(ESP_RESET_GPIO_Port,ESP_RESET_Pin,GPIO_PIN_SET);
+	HAL_Delay(1000);
+	HAL_UART_Receive_IT(&huart1,&Nhanbuff,1);
+	wifi_protocol_init();
+}
